eff724e3e64dfba6ed0e2112f664946a4cb4936d
Felix <felix.wolff2@student.hpi.uni-potsdam.de>
2016-09-26 21:46:44 +0200

    Hehe, Aufgabe 1.2 gelöst

--- #1/endianchecker.c
+++ #1/endianchecker.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <arpa/inet.h>
 
 int main(int argc, char** argv) {
-  char endianteller = 1;
-  char* c_pointer   = &endianteller;
+  // convert to network byte order - and that is always big Endian
+  // Da hat Polze einen guten Tipp gegeben
   
-  if (c_pointer = 1) {
-    printf("Hello Little Endian World!");
+  if (htonl(123) == 123) {
+    printf("Hello Big Endian World!\n");
   } else {
-    printf("Hello Big Endian World!");
+    printf("Hello Little Endian World!\n");
   }
 }
